6cc4a2fde0f0214bbb01893b635b13a6aa4c5130
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-27 01:15:18 +0200

    add comments

--- src/magicserver.js
+++ src/magicserver.js
@@ -39,40 +39,56 @@ export default class Server {
     console.log(`start server`);
 
     createServer((req, res) => {
+      // Get startTime for logging
       const startTime = new Date().getTime();
+
       let url = req.url;
 
       const isLocalUrl = menuItems.filter(item => item.href === url).length;
+
+      // Return index.html for client side urls and root
       if (url === '/' || isLocalUrl) {
         url = '/index.html';
       }
 
+      // 404 if file does not have a mime-type
       if (!files[url] || !files[url].mime) {
         return this.error404(url, res);
       }
+
+      // Get original file mimetype
       const {mime} = files[url];
 
+      // Check if client accepts gzip
       const zipped = req.headers['accept-encoding'].indexOf('gzip') > -1;
 
+      // If accepted, set Content-Encoding and add .gz to url
       if (zipped) {
         url = `${url}.gz`;
         res.setHeader('Content-Encoding', 'gzip');
       }
 
+      // finally get the compressed or uncompressed file
       const file = files[url];
 
+      // 404 if file is empty
       if (!file.content) {
         return this.error404(url, res);
       }
 
+      // Set the Content-Type to the mime of the uncompressed file
       res.setHeader('Content-Type', mime);
 
+      // End the response with the file contents
       res.end(file.content);
 
       const endTime = new Date().getTime();
 
+      // log the url and response time
       console.log(`${url} ${endTime - startTime}ms`);
     })
+
+    // Listen to port set in config
     .listen(port, () => {
       console.log(`server listening to ${port}`);
     });
