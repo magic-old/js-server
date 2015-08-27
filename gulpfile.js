d155088021d357ede41eb6ac1a40ee8ab2860aff
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-27 02:28:55 +0200

    add build:bin to tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,26 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var del = require('del');
 var watch = require('gulp-watch');
+var chmod = require('gulp-chmod');
 
 gulp.task('clean', function(cb) {
   var dist = [
     'magicserver.js',
+    'bin.js',
   ];
   del(dist, cb);
 });
 
-gulp.task('build', ['clean'], function () {
-  return gulp.src(join('src', '**'))
+gulp.task('build', ['clean', 'build:bin'], function () {
+  return gulp.src(join('src', 'magicserver.js'))
     .pipe(babel())
+    .pipe(gulp.dest(__dirname));
+});
+
+gulp.task('build:bin', function() {
+  return gulp.src(join('src', 'bin.js'))
+    .pipe(babel())
+    .pipe(chmod(755))
     .pipe(gulp.dest(__dirname));
 });
 
