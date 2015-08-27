29782a2c8fa6da77262010503aae075527a2ae65
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-27 06:06:20 +0200

    add magic/logger

--- src/magicserver.js
+++ src/magicserver.js
@@ -3,19 +3,19 @@ import {readdirSync, readFileSync, statSync} from 'fs-extra';
 import {createServer} from 'http';
 import {sync} from 'glob';
 import mime from 'mime';
-import color from 'bash-color';
+import log from 'magiclogger';
 
-export class Server {
+class MagicServer {
   constructor(configPath = join(process.cwd(), 'config.js')) {
     this.config = require(configPath);
 
     const {dirs, files, server} = this.config;
 
     const dir = join(dirs.out, '**', server.files);
-    log.info(`collecting files in ${dir}`);
+    log(`collecting files in ${dir}`);
 
     const globbed = sync(dir, {nodir: true});
-    log.info(`found ${globbed.length} files`);
+    log(`found ${globbed.length} files`);
 
     const collectedFiles = this.collectFiles(globbed);
     this.serve(collectedFiles);
@@ -35,8 +35,8 @@ export class Server {
   }
 
   serve(files) {
-    const {port, menuItems} = this.config;
-    log.info(`start server`);
+    const {port, menuItems, pageItems} = this.config;
+    log(`start server`);
 
     createServer((req, res) => {
       // Get startTime for logging
@@ -44,36 +44,54 @@ export class Server {
 
       let url = req.url;
 
+      if(url.length > 1 && url.substr(url.length - 1) === '/') {
+        url = url.substr(0, url.length - 1);
+      }
+
       const isLocalUrl = menuItems.filter(item => item.href === url).length;
+      const isPageUrl = Object.keys(pageItems).filter(key => key === url).length;
+
+      let file = files[url];
 
       // Return index.html for client side urls and root
       // ♥ = %E2%99%A5
-      if (url === '/' || url === '/%E2%99%A5' || isLocalUrl) {
+      if (isLocalUrl) {
         url = '/index.html';
+        file = files[url];
+      } else if (isPageUrl) {
+        const pageItemUrl = pageItems[url];
+        const pageItemFile = files[pageItemUrl];
+
+        if (pageItemFile && pageItemFile.mime) {
+          url = pageItemUrl;
+          file = pageItemFile;
+        }
       }
 
-      // 404 if file does not have a mime-type
-      if (!files[url] || !files[url].mime) {
+      // 404 if file is empty
+      if (!file || !file.content) {
         return this.error404(url, res);
       }
 
       // Get original file mimetype
-      const {mime} = files[url];
+      const {mime} = file;
+
+      let zippedFile = null;
 
       // Check if client accepts gzip
       const zipped = req.headers['accept-encoding'].indexOf('gzip') > -1;
 
       // If accepted, set Content-Encoding and add .gz to url
       if (zipped) {
-        url = `${url}.gz`;
-        res.setHeader('Content-Encoding', 'gzip');
+        const testUrl = `${url}.gz`;
+        if (files[testUrl] && files[testUrl].content) {
+          res.setHeader('Content-Encoding', 'gzip');
+          file = files[testUrl];
+        }
       }
 
-      // finally get the compressed or uncompressed file
-      const file = files[url];
-
       // 404 if file is empty
-      if (!file.content) {
+      if (!file || !file.content) {
         return this.error404(url, res);
       }
 
@@ -84,58 +102,22 @@ export class Server {
       res.end(file.content);
 
       const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-      log.request(ip, url, startTime);
+
+      const endTime = process.hrtime(startTime);
+      log.request(ip, url, startTime, endTime);
     })
 
     // Listen to port set in config
     .listen(port, () => {
-      log.success(`server listening to ${port}`);
+      log.success(`server listening to localhost:${port}`);
     });
   }
 
   error404(url, res) {
-    log.error(`could not find file: ${url}`);
+    log.error(`could not find file with url: ${url}`);
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('File not found');
   }
 }
 
-class MagicLogger {
-  info(val) {
-    console.log(val);
-  }
-  success(val) {
-    console.log(color.green(val));
-  }
-  error(val) {
-    console.log(color.red(val));
-  }
-
-  request(ip, url, start) {
-    const end = process.hrtime(start);
-
-    // log the url and response time
-    const nanoseconds = ((end[0] * 1e9) + end[1]);
-    let displayedTime = nanoseconds;
-    let timeUnit = 'ns';
-
-    if (displayedTime > 1000) {
-      displayedTime = displayedTime / 1000;
-      timeUnit = 'µs';
-    }
-
-    if (displayedTime > 1000) {
-      displayedTime = displayedTime / 1000;
-      timeUnit = 'ms';
-    }
-
-    if (displayedTime > 300 && timeUnit === 'ms') {
-      log.error(`${url} Response time over 300ms`);
-    }
-    log.success(`${ip} - ${url} - ${parseInt(displayedTime)}${timeUnit}`);
-  }
-}
-
-const log = new MagicLogger();
-
-export default new Server();
+export default new MagicServer();
